c7de0100d90db2fce32baa7ee327b55ef53134d3
Yunkeun <yunx2@naver.com>
2021-12-30 18:33:17 +0900

    이코테 구현 4-2

---
+++ ThisIsCodingTest/구현/4-2.py
@@ -0,0 +1,18 @@
+# 시각
+h = int(input())
+m, n = 59, 59
+hour, minute, second = 0, 0, 0
+count = 0
+while (not(hour == h and minute == m and second == n)):
+    print(hour, minute, second)
+    if (second == 60):
+        minute += 1
+        second = 0
+    if (minute == 60):
+        hour += 1
+        minute = 0
+    if (str(second).find("3") != -1 or str(minute).find("3") != -1 or str(hour).find("3") != -1):
+        print("inside : ", hour, minute, second)
+        count += 1
+    second += 1
+print(count)
