9aef211a48ca4292ba2e936320346bf02786accf
Yunkeun <yunx2@naver.com>
2022-01-13 01:07:39 +0900

    Add Boj 토마토 (수정)

--- BaekJoon/DFS와 BFS/7576_토마토/solve.py
+++ BaekJoon/DFS와 BFS/7576_토마토/solve.py
@@ -1,35 +1,33 @@
 from collections import deque
+
 m, n = map(int, input().split())
-s = []
+
+graph = [list(map(int, input().split())) for _ in range(n)]
 queue = deque()
-dx = [1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-for i in range(n):
-    s.append(list(map(int, input().split())))
-def bfs():
-    while queue:
-        a, b = queue.popleft()
-        for i in range(4):
-            x = a + dx[i]
-            y = b + dy[i]
-            if 0 <= x < n and 0 <= y < m and s[x][y] == 0:
-                s[x][y] = s[a][b] + 1
-                queue.append([x, y])
+result = 0
+
 for i in range(n):
     for j in range(m):
-        if s[i][j] == 1:
-            queue.append([i, j])
+        if graph[i][j] == 1:
+            queue.append((i, j))
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
 bfs()
-isTrue = False
-result = -2
-for i in s:
-    for j in i:
-        if j == 0:
-            isTrue = True
-        result = max(result, j)
-if isTrue == True:
-    print(-1)
-elif result == -1:
-    print(0)
-else:
-    print(result - 1)
+for row in graph:
+    for x in row:
+        if x == 0:
+            print(-1)
+            exit(0)
+    result = max(result, max(row))
+print(result - 1)
