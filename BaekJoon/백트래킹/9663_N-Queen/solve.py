8cd69cd0802f0ccc837713119df4f99d6115e223
Yunkeun <yunx2@naver.com>
2022-07-29 13:14:38 +0900

    Add boj 9663

--- BaekJoon/백트래킹/9663_N-Queen/solve.py
+++ BaekJoon/백트래킹/9663_N-Queen/solve.py
@@ -1,21 +1,26 @@
 def isPromising(x):
     for i in range(x):
-        if row[x] == row[i] or abs(x-i) == abs(row[x] - row[i]): # 같은 행에 있거나
+        if row[i] == row[x] or abs(x-i) == abs(row[x] - row[i]):
             return False
     return True
 
 def nQueens(x):
-    global ans
-    if x == n:
-        ans += 1
+    global answer
+    if n == x:
+        answer += 1
         return
     for i in range(n):
+        if visited[i]:
+            continue
         row[x] = i
         if isPromising(x):
+            visited[i] = True
             nQueens(x+1)
+            visited[i] = False
 
 n = int(input())
 row = [0] * n
-ans = 0
+visited = [False] * n
+answer = 0
 nQueens(0)
-print(ans)
+print(answer)
