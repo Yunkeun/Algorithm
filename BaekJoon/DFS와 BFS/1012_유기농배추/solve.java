7f4a565071035eff828da8371d732b06d185e256
Yunkeun <yunx2@naver.com>
2022-09-02 11:51:51 +0900

    Add boj 유기농배추java

--- BaekJoon/DFS와 BFS/1012_유기농배추/solve.java
+++ BaekJoon/DFS와 BFS/1012_유기농배추/solve.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -6,38 +5,29 @@ import static java.lang.Integer.*;
 
 public class Main {
 
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int[][] graph;
+    private static boolean[][] visited;
+    private static int row;
+    private static int column;
     private static final int[] dx = {1, -1, 0, 0};
     private static final int[] dy = {0, 0, 1, -1};
-    private static int row;
-    private static int col;
 
     public static void main(String[] args) throws IOException {
-        int numberOfTests = parseInt(br.readLine());
-        for (int i = 0; i < numberOfTests; i++) {
-            int minimumNumberOfWorm = test();
-            System.out.println(minimumNumberOfWorm);
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int numberOfTestCase = parseInt(br.readLine());
+        for (int i = 0; i < numberOfTestCase; i++) {
+            final int count = getCount(br);
+            System.out.println(count);
         }
     }
 
-    private static int test() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        row = parseInt(st.nextToken());
-        col = parseInt(st.nextToken());
-        int numberOfCabbage = parseInt(st.nextToken());
-        int[][] field = new int[row][col];
-        for (int i = 0; i < numberOfCabbage; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int x = parseInt(st2.nextToken());
-            int y = parseInt(st2.nextToken());
-            field[x][y] = 1;
-        }
+    private static int getCount(BufferedReader br) throws IOException {
+        setGraph(br);
         int count = 0;
-        boolean[][] visited = new boolean[row][col];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (field[i][j] == 1 && !visited[i][j]) {
-                    dfs(i, j, visited, field);
+        for (int m = 0; m < row; m++) {
+            for (int n = 0; n < column; n++) {
+                if (graph[m][n] == 1 && !visited[m][n]) {
+                    dfs(m, n);
                     count++;
                 }
             }
@@ -45,14 +35,29 @@ public class Main {
         return count;
     }
 
-    private static void dfs(int x, int y, boolean[][] visited, int[][] field) {
+    private static void setGraph(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        row = parseInt(st.nextToken());
+        column = parseInt(st.nextToken());
+        graph = new int[row][column];
+        visited = new boolean[row][column];
+        final int numberOfCabbage = parseInt(st.nextToken());
+        for (int j = 0; j < numberOfCabbage; j++) {
+            st = new StringTokenizer(br.readLine());
+            final int x = parseInt(st.nextToken());
+            final int y = parseInt(st.nextToken());
+            graph[x][y] = 1;
+        }
+    }
+
+    private static void dfs(int x, int y) {
         visited[x][y] = true;
-        for (int i = 0; i < dx.length; i++) {
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (0 <= nx && nx < row && 0 <= ny && ny < col) {
-                if (field[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny, visited, field);
+            if (0 <= nx && nx < row && 0 <= ny && ny < column) {
+                if (graph[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
                 }
             }
         }
