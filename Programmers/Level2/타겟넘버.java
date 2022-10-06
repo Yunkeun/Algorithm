24d69a26e1811602cc3165f1be1a9384b796c61f
Yunkeun <yunx2@naver.com>
2022-10-06 21:54:10 +0900

    Add prgrms 타겟넘버java

---
+++ Programmers/Level2/타겟넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    private static int answer;
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        dfs(0, numbers, target, 0);
+        return answer;
+    }
+    
+    private void dfs(int depth, int[] numbers, int target, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);
+    }
+}
